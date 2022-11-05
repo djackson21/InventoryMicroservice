acd14a768279989a158e8ddcfa5bf983c044856a
Derron Jackson <Derrnjackson01gmail.com>
2022-11-05 14:51:21 -0500

    Created Order and Inventory Service Module.

---
+++ inventory-service/src/main/java/com/minoritypioneer/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.minoritypioneer.inventoryservice;
+
+import com.minoritypioneer.inventoryservice.model.Inventory;
+import com.minoritypioneer.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
