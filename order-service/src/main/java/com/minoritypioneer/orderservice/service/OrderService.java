36f7a1ac7142d3bf6d2b900b167791b6293cf772
Derron Jackson <Derrnjackson01gmail.com>
2022-11-06 16:18:07 -0600

    Created Order and Inventory Service Module.

--- order-service/src/main/java/com/minoritypioneer/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/minoritypioneer/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.minoritypioneer.orderservice.service;
 
+import com.minoritypioneer.orderservice.dto.InventoryResponse;
 import com.minoritypioneer.orderservice.dto.OrderLineItemsDto;
 import com.minoritypioneer.orderservice.dto.OrderRequest;
 import com.minoritypioneer.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.minoritypioneer.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -29,6 +33,25 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
         orderRepository.save(order);
     }
 
